2588ccd094039bb1c818ead5a1804e4d3589f905
AJ Seelund <aj@seelund.com>
2016-12-03 13:41:24 -0500

    formatting

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,22 @@
 'use strict';
 
-angular.module('wwwApp', ['wwwApp.auth', 'wwwApp.admin', 'wwwApp.constants', 'ngCookies',
-    'ngResource', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'validation.match'
+angular.module('wwwApp'
+	,[ 'wwwApp.auth'
+	 , 'wwwApp.admin'
+	 , 'wwwApp.constants'
+	 , 'ngCookies'
+	 , 'ngResource'
+	 , 'ngSanitize'
+	 , 'ui.router'
+	 , 'ui.bootstrap'
+	 , 'validation.match'
   ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function
+  	( $urlRouterProvider
+  	, $locationProvider)
+  {
+    
     $urlRouterProvider.otherwise('/');
-
     $locationProvider.html5Mode(true);
+
   });
